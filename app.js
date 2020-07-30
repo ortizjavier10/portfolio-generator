4c9b41d7c3737fefc3d19af20f21d03e63ac1433
JAVI <ortiz.javier10@gmail.com>
2020-07-29 21:46:04 -0400

    html template update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
 
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 
@@ -142,5 +142,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err);
+
+          console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
